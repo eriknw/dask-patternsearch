20e5e83613be53f5c6290ee83f578c4dc14de334
Erik Welch <erik.n.welch@gmail.com>
2017-04-09 13:14:59 -0500

    Add tests and travis-ci.  Also, some logic reorganization and fixes.

--- dask_patternsearch/search.py
+++ dask_patternsearch/search.py
@@ -6,7 +6,7 @@ import distributed
 import numpy as np
 
 from time import time
-from toolz import concat, cons, take
+from toolz import concat, take
 
 from .stencil import RightHandedSimplexStencil
 
@@ -18,6 +18,7 @@ class Point(object):
         self.point = point
         self.halvings = halvings
         self.is_accepted = False
+        self.stop_time = None
 
     def __hash__(self):
         return hash(self.point.tostring())
@@ -39,13 +40,13 @@ def randomize_stencil(dims, it):
     return concat(randomize_chunk(i, it) for i in itertools.count(2*dims, dims))
 
 
-def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_new_submit=0, randomize=False, max_stencil_size=None, stopratio=0.01, max_tasks=None, max_time=None):
+def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_new_submit=0, randomize=True, max_stencil_size=None, stopratio=0.01, max_tasks=None, max_time=None):
     # bound=None, low_memory_stencil=False
     if queue_size is None:
         ncores = client.ncores()
         queue_size = sum(ncores.values()) + len(ncores)
     if min_queue_size is None:
-        min_queue_size = queue_size // 2
+        min_queue_size = max(1, queue_size // 2)
     if max_stencil_size is None:
         max_stencil_size = 1e9
     dims = len(stepsize)
@@ -58,15 +59,18 @@ def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_
 
     orientation = np.ones(dims)
     cur_point = Point(to_grid(x), -1)
+    cur_point.start_time = time()
+    cur_point.parent = cur_point
     cur_cost = result = np.inf
+    is_contraction = True
     new_point = None
-    is_finished = False
 
     if max_time is not None:
         end_time = time() + max_time
     results = {}
     running = {}
     processing = []
+    contract_conditions = set()
     next_point = None
     next_cost = None
 
@@ -75,8 +79,8 @@ def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_
     as_completed = distributed.as_completed([future], with_results=True)
     running[future] = cur_point
     results[cur_point] = None
-    is_contraction = True
 
+    is_finished = False
     while not is_finished or running or next_point is not None:
         if max_time is not None and time() > end_time:
             is_finished = True
@@ -84,18 +88,22 @@ def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_
         # Initialize new point
         if new_point is not None or is_contraction:
             if is_contraction:
-                cur_point.halvings += 1
-                cur_point.parent = cur_point
                 is_contraction = False
-            else:
-                # TODO: if parent.is_accepted and gparent.is_accepted and parent.cost < gparent.cost: DOUBLE
-                # TODO: also look at (2*new_point - prev_point) and double as appropriate
-                cur_point = new_point
-                cur_cost = result
-                new_point = None
+                if cur_point.stop_time is None:
+                    cur_point.stop_time = time()
+                new_point = Point(cur_point.point, cur_point.halvings + 1)
+                new_point.parent = cur_point
+                new_point.is_accepted = True
+                new_point.result = cur_cost
+                new_cost = cur_cost
+                new_point.start_time = time()
+            cur_point = new_point
+            cur_cost = new_cost
+            new_point = None
+            new_cost = None
             cur_stepsize = to_grid(orientation * stepsize / 2.**cur_point.halvings)
             cur_added = 0
-            contract_conditions = set()
+            contract_conditions.clear()
             it = stencil.generate_stencil_points()
             if randomize:
                 it = randomize_stencil(dims, it)
@@ -156,47 +164,61 @@ def search(client, func, x, stepsize, queue_size=None, min_queue_size=None, min_
                 continue
 
         # Collect all completed tasks, or wait for one if nothing else to do
-        block = (
-            len(running) >= queue_size
-            or (
-                next_point is None
-                and (is_finished or stencil_index >= max_stencil_size)
+        if running:
+            block = (
+                len(running) >= queue_size
+                or (
+                    next_point is None
+                    and (is_finished or stencil_index >= max_stencil_size)
+                )
             )
-        )
-        for future, result in as_completed.next_batch(block=block):
-            point = running.pop(future)
-            point.stop_time = time()
-            if next_point is None:
-                next_point = point
-                next_cost = result
-            elif result < next_cost:
-                processing.append((next_point, next_cost))
-                next_point = point
-                next_cost = result
-            else:
-                processing.append((point, result))
+
+            for future, result in as_completed.next_batch(block=block):
+                point = running.pop(future)
+                point.stop_time = time()
+                if next_point is None:
+                    next_point = point
+                    next_cost = result
+                elif result < next_cost:
+                    processing.append((next_point, next_cost))
+                    next_point = point
+                    next_cost = result
+                else:
+                    processing.append((point, result))
 
         # Process all results
         # Be greedy: the new point will be the result with the lowest cost.
         # It's possible one could want a different policy to better explore
         # around multiple minima.
-        if next_point is not None and (cur_added >= min_new_submit or stencil_index >= max_stencil_size):
-            for point, result in cons((next_point, next_cost), processing):
+        if next_point is not None and (cur_added >= min_new_submit or stencil_index >= max_stencil_size or is_finished):
+            results[next_point] = next_cost
+            next_point.result = next_cost
+            contract_conditions.discard(next_point)
+            if next_cost < cur_cost:
+                next_point.is_accepted = True
+                # Orient the asymmetric stencil towards the expected direction
+                # of descent based on the old and new points and their parents.
+                # The stencil prefers negative directions, so this is the correct sign.
+                diff = (
+                    (next_point.point - next_point.parent.point)
+                    + (cur_point.point - cur_point.parent.point)
+                )
+                orientation = np.where(diff, np.copysign(orientation, diff), orientation)
+                new_point = next_point
+                new_cost = next_cost
+
+            for point, result in processing:
                 results[point] = result
                 point.result = result
-                if result < cur_cost:
-                    point.is_accepted = True
-                    # The stencil prefers negative directions, so this is the correct sign
-                    diff = point.point - cur_point.point
-                    orientation = np.where(diff, np.copysign(orientation, diff), orientation)
-                    new_point = point
-                elif new_point is not None and not is_finished:
-                    if point in contract_conditions:
-                        contract_conditions.remove(point)
-                    if not contract_conditions and stencil_index > 2 * dims:
-                        is_contraction = True
+                contract_conditions.discard(point)
+            if new_point is None and not contract_conditions and stencil_index > 2 * dims:
+                is_contraction = True
             next_point = None
             next_cost = None
             processing.clear()
+        elif next_point is None and stencil_index >= max_stencil_size and not running and not is_finished:
+            # Nothing running, nothing to process, and nothing to submit, so contract
+            is_contraction = True
+
     return cur_point, results
 
---
+++ dask_patternsearch/tests/test_search.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import time
+from distributed import Client
+from distributed.utils_test import cluster, loop
+
+from dask_patternsearch import search
+
+
+def sphere(x):
+    return x.dot(x)
+
+
+def test_convergence_2d_simple(loop):
+    with cluster() as (s, [a, b]):
+        with Client(s['address'], loop=loop) as client:
+            x0 = np.array([10., 15])
+            stepsize = np.array([1., 1])
+            stopratio = 1e-2
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, queue_size=20)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, queue_size=1)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, min_new_submit=4)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, max_tasks=10)
+            assert len(results) == 10
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, max_stencil_size=4)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
+            best, results = search(client, sphere, x0, stepsize, stopratio=stopratio, max_stencil_size=4, min_new_submit=4)
+            assert (np.abs(best.point) < 2*stopratio).all()
+
